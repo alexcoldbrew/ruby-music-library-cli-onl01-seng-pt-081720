0e3aadb219097853107e1331bab1125afe53804e
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-09-26 00:37:52 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -32,6 +32,12 @@ class MusicLibraryController
     end
   end
   
+  def list_artists
+    Artist.all.sort{|a, b| a.name <=> b.name}.each_with_index do |a, i|
+      puts "#{i+1}. #{a.name}"
+    end
+  end
+  
   
   
 end
